779e8066c6f3911ebda6923e6643e768bae81e48
anushree10000 <anushreedixit16@gmail.com>
2026-05-02 11:43:48 +0530

    Add notification route for sending notifications

--- notification_app_be/route/notificationRoute.js
+++ notification_app_be/route/notificationRoute.js
@@ -1,1 +1,9 @@
 
+const express = require("express");
+const router = express.Router();
+
+const sendNotification = require("../service/notificationService");
+
+router.post("/send", sendNotification);
+
+module.exports = router;
