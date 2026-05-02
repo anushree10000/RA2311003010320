a00923edc423b5d5d46bf82dd5ba208bd8bc2618
anushree10000 <anushreedixit16@gmail.com>
2026-05-02 11:01:14 +0530

    Update logger.js

--- logging_middleware/utils/logger.js
+++ logging_middleware/utils/logger.js
@@ -1,1 +1,26 @@
+const axios = require("axios");
 
+const LOG_API = "http://20.207.122.201/evaluation-service/logs";
+
+async function Log(stack, level, pkg, message, token) {
+  try {
+    await axios.post(
+      LOG_API,
+      {
+        stack,
+        level,
+        package: pkg,
+        message
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+  } catch (err) {
+    console.error("Logging failed:", err.message);
+  }
+}
+
+module.exports = Log;
