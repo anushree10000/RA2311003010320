8d2dd5be535bafab8e52d0d8b8443b211c6a7686
anushree10000 <anushreedixit16@gmail.com>
2026-05-02 12:20:13 +0530

    Update notificationService.js

--- notification_app_be/service/notificationService.js
+++ notification_app_be/service/notificationService.js
@@ -3,17 +3,16 @@ const Log = require("../../logging_middleware/utils/logger");
 const sendNotification = async (req, res) => {
   const { type, message } = req.body;
 
-  // Priority logic
   let priority;
   if (type === "urgent") priority = "HIGH";
   else if (type === "warning") priority = "MEDIUM";
   else priority = "LOW";
 
-  // ❗ DO NOT await this
+  
   Log("backend", "info", "service", `Message: ${message}`, "dummy-token")
-    .catch(() => {}); // ignore failure
+    .catch(() => {}); 
 
-  // Always respond success
+  
   res.json({
     success: true,
     priority,
