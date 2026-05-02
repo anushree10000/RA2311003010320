a8ab642c66a63b38e91d9aa771bbea5cc220d5a7
anushree10000 <anushreedixit16@gmail.com>
2026-05-02 11:42:32 +0530

    Set up Express server with notification route

--- notification_app_be/server.js
+++ notification_app_be/server.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const notificationRoute = require("./route/notificationRoute");
 
+const app = express();
+app.use(express.json());
+
+app.use("/notifications", notificationRoute);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
